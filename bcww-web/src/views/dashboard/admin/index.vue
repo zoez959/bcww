235477a68432c72021ab274f24c513affe647072
zoujingyi <zoujy2@cjsc.com>
2023-08-01 22:06:39 +0800

    Initial commit

---
+++ bcww-web/src/views/dashboard/admin/index.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="dashboard-editor-container">
+    <!--    <github-corner class="github-corner" />-->
+
+    <panel-group @handleSetLineChartData="handleSetLineChartData"/>
+
+    <el-row style="background:#fff; padding:16px 16px 0; margin-bottom:32px;">
+      <el-col :xs="24" :sm="24" :lg="24">
+        <line-chart :chart-data="lineChartData"/>
+      </el-col>
+    </el-row>
+
+    <!--    <el-row style="background:#fff;padding:16px 16px 0;margin-bottom:32px;">-->
+    <!--      <line-chart :chart-data="lineChartData"/>-->
+    <!--    </el-row>-->
+
+    <!--个人积分结构待调整-->
+    <el-row :gutter="32">
+      <!--      <el-col :xs="12" :sm="12" :lg="6">-->
+      <!--        <div class="chart-wrapper">-->
+      <!--          <panel-group />-->
+      <!--        </div>-->
+      <!--      </el-col>-->
+      <el-col :xs="24" :sm="24" :lg="8">
+        <div class="chart-wrapper">
+          加分构成
+          <pie-chart/>
+        </div>
+      </el-col>
+      <el-col :xs="24" :sm="24" :lg="8">
+        <div class="chart-wrapper">
+          扣分构成
+          <pie-chart/>
+        </div>
+      </el-col>
+      <el-col :xs="24" :sm="24" :lg="8">
+        <div class="chart-wrapper">
+          <box-card/>
+        </div>
+      </el-col>
+    </el-row>
+
+
+    <!--数据表展示 移到管理者界面-->
+    <!--    <el-row style="background:#fff;padding:16px 16px 0;margin-bottom:32px;">-->
+    <!--      <transaction-table/>-->
+    <!--    </el-row>-->
+
+    <!--    <el-row :gutter="8">-->
+    <!--      &lt;!&ndash;      <el-col :xs="{span: 24}" :sm="{span: 24}" :md="{span: 24}" :lg="{span: 12}" :xl="{span: 12}" style="padding-right:8px;margin-bottom:30px;">&ndash;&gt;-->
+    <!--      &lt;!&ndash;        <transaction-table />&ndash;&gt;-->
+    <!--      &lt;!&ndash;      </el-col>&ndash;&gt;-->
+    <!--      <el-col :xs="{span: 24}" :sm="{span: 12}" :md="{span: 12}" :lg="{span: 6}" :xl="{span: 6}"-->
+    <!--              style="margin-bottom:30px;">-->
+    <!--        <box-card/>-->
+    <!--      </el-col>-->
+    <!--      <el-col :xs="{span: 24}" :sm="{span: 12}" :md="{span: 12}" :lg="{span: 6}" :xl="{span: 6}"-->
+    <!--              style="margin-bottom:30px;">-->
+    <!--        <box-card/>-->
+    <!--      </el-col>-->
+    <!--    </el-row>-->
+  </div>
+
+
+
+</template>
+
+<script>
+import PanelGroup from './components/PanelGroup'
+import LineChart from './components/LineChart'
+import RaddarChart from './components/RaddarChart'
+import PieChart from './components/PieChart'
+import BarChart from './components/BarChart'
+import TransactionTable from './components/TransactionTable'
+import TodoList from './components/TodoList'
+import BoxCard from './components/BoxCard'
+
+const lineChartData = {
+  newVisitis: {
+    expectedData: [100, 120, 161, 134, 105, 160, 165],
+    actualData: [120, 82, 91, 154, 162, 140, 145]
+  },
+  messages: {
+    expectedData: [200, 192, 120, 144, 160, 130, 140],
+    actualData: [180, 160, 151, 106, 145, 150, 130]
+  },
+  purchases: {
+    expectedData: [80, 100, 121, 104, 105, 90, 100],
+    actualData: [120, 90, 100, 138, 142, 130, 130]
+  },
+  shoppings: {
+    expectedData: [130, 140, 141, 142, 145, 150, 160],
+    actualData: [120, 82, 91, 154, 162, 140, 130]
+  }
+}
+
+export default {
+  name: 'DashboardAdmin',
+  components: {
+    PanelGroup,
+    LineChart,
+    RaddarChart,
+    PieChart,
+    BarChart,
+    TransactionTable,
+    BoxCard
+  },
+  data() {
+    return {
+      lineChartData: lineChartData.newVisitis
+    }
+  },
+  methods: {
+    handleSetLineChartData(type) {
+      this.lineChartData = lineChartData[type]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard-editor-container {
+  padding: 32px;
+  background-color: rgb(240, 242, 245);
+  position: relative;
+
+  .chart-wrapper {
+    background: #fff;
+    padding: 16px 16px 0;
+    margin-bottom: 32px;
+  }
+}
+
+@media (max-width: 1024px) {
+  .chart-wrapper {
+    padding: 8px;
+  }
+}
+</style>
